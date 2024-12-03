537c24ee88120281100b66b6542a550fee0e049f
Kanish Kumar <kanishkumar11@outlook.in>
2024-12-03 17:39:39 +0530

    minor fix

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -9,7 +9,7 @@ import { surveyQuestions } from "@/lib/data";
 export default function Main() {
   const [currentSet, setCurrentSet] = useState(0);
   const [showEyeMeasurements, setShowEyeMeasurements] = useState(false);
-  const [showPreview, setShowPreview] = useState(true);
+  const [showPreview, setShowPreview] = useState(false);
   const [showSuccess, setShowSuccess] = useState(false);
   const [answers, setAnswers] = useState({});
   const [eyeMeasurements, setEyeMeasurements] = useState({
